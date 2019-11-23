0fafa57d623769943a94ce50dabdf2873343e90c
pavel <pavel.varentsov@citronium.com>
2019-11-23 16:39:53 +0300

    Code refactoring

--- nodejs-rest-client/src/bootstrap.ts
+++ nodejs-rest-client/src/bootstrap.ts
@@ -5,38 +5,46 @@ import { cpus } from 'os';
 import { CoreLogger } from './core/logger';
 
 (async (): Promise<void> => {
-
     if (!Config.API_CLUSTER_ENABLE) {
         const serverApplication: ServerApplication = ServerApplication.create();
         await serverApplication.bootstrap();
 
     } else {
-
-        if (isMaster) {
-            const cpuCount: number = cpus().length;
-
-            for (let workerIndex: number = 0; workerIndex < cpuCount; workerIndex++) {
-                fork();
-            }
-
-            on('exit', (worker: Worker, code: number, signal: string): void => {
-                const codeMessagePart: string = `code: ${code || 'unknown'}`;
-                const signalMessagePart: string = `signal: ${signal || 'unknown'}`;
-
-                const errorMassage: string =
-                    `Worker on PID: ${worker.process.pid} ` +
-                    `is down with ${codeMessagePart}; ${signalMessagePart}`;
-
-                CoreLogger.error(errorMassage);
-
-                fork();
-            });
-
-        } else {
-
-            const serverApplication: ServerApplication = ServerApplication.create();
-            await serverApplication.bootstrap();
-        }
+        await spawnWorkers();
+        await runApplicationOnWorkers();
     }
 
 })();
+
+async function runApplication(): Promise<void> {
+    const serverApplication: ServerApplication = ServerApplication.create();
+    await serverApplication.bootstrap();
+}
+
+async function spawnWorkers(): Promise<void> {
+    if (isMaster) {
+        const cpuCount: number = cpus().length;
+
+        for (let workerIndex: number = 0; workerIndex < cpuCount; workerIndex++) {
+            fork();
+        }
+
+        on('exit', (worker: Worker, code: number, signal: string): void => {
+            const codeMessagePart: string = `code: ${code || 'unknown'}`;
+            const signalMessagePart: string = `signal: ${signal || 'unknown'}`;
+
+            const errorMassage: string =
+                `Worker on PID: ${worker.process.pid} ` +
+                `is down with ${codeMessagePart}; ${signalMessagePart}`;
+
+            CoreLogger.error(errorMassage);
+            fork();
+        });
+    }
+}
+
+async function runApplicationOnWorkers(): Promise<void> {
+    if (!isMaster) {
+        await runApplication();
+    }
+}
