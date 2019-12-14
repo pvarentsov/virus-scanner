ac030f7d8241b20a4abcc66a83f8c97ee802e874
pavel <pavel.varentsov@citronium.com>
2019-12-15 02:09:25 +0300

    Updated tests' descriptions

--- nodejs-rest-client/test/unit/core/configuration/config/Config.spec.ts
+++ nodejs-rest-client/test/unit/core/configuration/config/Config.spec.ts
@@ -4,47 +4,47 @@ describe('Config', () => {
 
     describe('Positive cases', () => {
 
-        it('Should be defined with API_HOST variable', () => {
+        it('Should be initialized with API_HOST variable', () => {
             expect(typeof Config.API_HOST === 'string').toBeTruthy();
         });
 
-        it('Should be defined with API_PORT variable', () => {
+        it('Should be initialized with API_PORT variable', () => {
             expect(typeof Config.API_PORT === 'number').toBeTruthy();
         });
 
-        it('Should be defined with API_BASE_PATH variable', () => {
+        it('Should be initialized with API_BASE_PATH variable', () => {
             expect(typeof Config.API_BASE_PATH === 'string').toBeTruthy();
         });
 
-        it('Should be defined with API_DOCUMENTATION_HOST variable', () => {
+        it('Should be initialized with API_DOCUMENTATION_HOST variable', () => {
             expect(typeof Config.API_DOCUMENTATION_HOST === 'string').toBeTruthy();
         });
 
-        it('Should be defined with API_CLUSTER_ENABLE variable', () => {
+        it('Should be initialized with API_CLUSTER_ENABLE variable', () => {
             expect(typeof Config.API_CLUSTER_ENABLE === 'number').toBeTruthy();
         });
 
-        it('Should be defined with CLAMAV_HOST variable', () => {
+        it('Should be initialized with CLAMAV_HOST variable', () => {
             expect(typeof Config.CLAMAV_HOST === 'string').toBeTruthy();
         });
 
-        it('Should be defined with CLAMAV_PORT variable', () => {
+        it('Should be initialized with CLAMAV_PORT variable', () => {
             expect(typeof Config.CLAMAV_PORT === 'number').toBeTruthy();
         });
 
-        it('Should be defined with CLAMAV_TIMEOUT variable', () => {
+        it('Should be initialized with CLAMAV_TIMEOUT variable', () => {
             expect(typeof Config.CLAMAV_TIMEOUT === 'number').toBeTruthy();
         });
 
-        it('Should be defined with LOG_FORMAT variable', () => {
+        it('Should be initialized with LOG_FORMAT variable', () => {
             expect(typeof Config.LOG_FORMAT === 'string').toBeTruthy();
         });
 
-        it('Should be defined with LOG_DISABLE_COLORS variable', () => {
+        it('Should be initialized with LOG_DISABLE_COLORS variable', () => {
             expect(typeof Config.LOG_DISABLE_COLORS === 'number').toBeTruthy();
         });
 
-        it('Should be defined with MAX_SYNC_SCAN_FILE_SIZE variable', () => {
+        it('Should be initialized with MAX_SYNC_SCAN_FILE_SIZE variable', () => {
             expect(typeof Config.MAX_SYNC_SCAN_FILE_SIZE === 'number').toBeTruthy();
         });
 
