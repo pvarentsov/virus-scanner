9a7ff4f33ae6da60c07d979fb12ffe08d178cf46
pavel <pavel.varentsov@citronium.com>
2019-12-15 15:53:10 +0300

    Using a single package.json in the root directory for rest client

--- nodejs-rest-client/test/unit/core/configuration/config/Config.spec.ts
+++ nodejs-rest-client/test/unit/core/configuration/config/Config.spec.ts
@@ -2,49 +2,49 @@ import { Config } from '../../../../../src/core/configuration';
 
 describe('Config', () => {
 
-    describe('Positive cases', () => {
+    describe(`Config's initialization with environment variables`, () => {
 
-        it('Should be initialized with API_HOST variable', () => {
+        it('When environment variables are exposed, expect API_HOST to be initialized as string', () => {
             expect(typeof Config.API_HOST === 'string').toBeTruthy();
         });
 
-        it('Should be initialized with API_PORT variable', () => {
+        it('When environment variables are exposed, expect API_PORT to be initialized as number', () => {
             expect(typeof Config.API_PORT === 'number').toBeTruthy();
         });
 
-        it('Should be initialized with API_BASE_PATH variable', () => {
+        it('When environment variables are exposed, expect API_BASE_PATH to be initialized as string', () => {
             expect(typeof Config.API_BASE_PATH === 'string').toBeTruthy();
         });
 
-        it('Should be initialized with API_DOCUMENTATION_HOST variable', () => {
+        it('When environment variables are exposed, expect API_DOCUMENTATION_HOST to be initialized as string', () => {
             expect(typeof Config.API_DOCUMENTATION_HOST === 'string').toBeTruthy();
         });
 
-        it('Should be initialized with API_CLUSTER_ENABLE variable', () => {
+        it('When environment variables are exposed, expect API_CLUSTER_ENABLE to be initialized as number', () => {
             expect(typeof Config.API_CLUSTER_ENABLE === 'number').toBeTruthy();
         });
 
-        it('Should be initialized with CLAMAV_HOST variable', () => {
+        it('When environment variables are exposed, expect CLAMAV_HOST to be initialized as string', () => {
             expect(typeof Config.CLAMAV_HOST === 'string').toBeTruthy();
         });
 
-        it('Should be initialized with CLAMAV_PORT variable', () => {
+        it('When environment variables are exposed, expect CLAMAV_PORT to be initialized as number', () => {
             expect(typeof Config.CLAMAV_PORT === 'number').toBeTruthy();
         });
 
-        it('Should be initialized with CLAMAV_TIMEOUT variable', () => {
+        it('When environment variables are exposed, expect CLAMAV_TIMEOUT to be initialized as number', () => {
             expect(typeof Config.CLAMAV_TIMEOUT === 'number').toBeTruthy();
         });
 
-        it('Should be initialized with LOG_FORMAT variable', () => {
+        it('When environment variables are exposed, expect LOG_FORMAT to be initialized as string', () => {
             expect(typeof Config.LOG_FORMAT === 'string').toBeTruthy();
         });
 
-        it('Should be initialized with LOG_DISABLE_COLORS variable', () => {
+        it('When environment variables are exposed, expect LOG_DISABLE_COLORS to be initialized as number', () => {
             expect(typeof Config.LOG_DISABLE_COLORS === 'number').toBeTruthy();
         });
 
-        it('Should be initialized with MAX_SYNC_SCAN_FILE_SIZE variable', () => {
+        it('When environment variables are exposed, expect MAX_SYNC_SCAN_FILE_SIZE to be initialized as number', () => {
             expect(typeof Config.MAX_SYNC_SCAN_FILE_SIZE === 'number').toBeTruthy();
         });
 
